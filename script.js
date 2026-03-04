e6733248b91d6fba92bbc5839488df9e17340eaf
g-akca <gok.akca66@gmail.com>
2026-03-04 04:39:52 +0300

    Add password generation script

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const password = document.getElementById("password");
 const copiedText = document.getElementById("copied-text");
 const copyBtn = document.querySelector("#result-div svg");
+
 const lengthInput = document.getElementById("length");
 const lengthText = document.getElementById("length-text");
+
+const uppercaseOption = document.getElementById("uppercase");
+const lowercaseOption = document.getElementById("lowercase");
+const numbersOption = document.getElementById("numbers");
+const symbolsOption = document.getElementById("symbols");
+
+const generateBtn = document.getElementById("generate-btn");
 
 copyBtn.addEventListener("click", () => {
     password.select();
@@ -13,4 +21,36 @@ copyBtn.addEventListener("click", () => {
 
 lengthInput.addEventListener("input", () => {
     lengthText.innerText = lengthInput.value;
+});
+
+generateBtn.addEventListener("click", () => {
+    const charPool = [];
+
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const numberChars = "0123456789";
+    const symbolChars = "!@#$%^&*()_+[]{}|;:,.<>?";
+
+    if (uppercaseOption.checked) charPool.push(...uppercaseChars);
+
+    if (lowercaseOption.checked) charPool.push(...lowercaseChars);
+
+    if (numbersOption.checked) charPool.push(...numberChars);
+
+    if (symbolsOption.checked) charPool.push(...symbolChars);
+
+    if (charPool.length === 0) {
+        alert("Please select at least one character type!");
+        return;
+    }
+
+    const length = lengthInput.value;
+    let generatedPassword = "";
+
+    for (let i = 0; i < length; i++) {
+        generatedPassword += charPool[Math.floor(Math.random() * charPool.length)];
+    }
+
+    password.value = generatedPassword;
+    copiedText.hidden = true;
 });
