dafbfc1ca1d214572841273723d1b5d17dd5d749
g-akca <gok.akca66@gmail.com>
2026-03-04 05:10:31 +0300

    Add strength calculation and bar update script

--- script.js
+++ script.js
@@ -10,7 +10,55 @@ const lowercaseOption = document.getElementById("lowercase");
 const numbersOption = document.getElementById("numbers");
 const symbolsOption = document.getElementById("symbols");
 
+const strengthText = document.getElementById("strength-text");
+const strengthBars = document.querySelectorAll(".strength-bar");
 const generateBtn = document.getElementById("generate-btn");
+
+function resetStrengthBars() {
+    strengthBars.forEach(bar => bar.className = "strength-bar");
+}
+
+function updateStrength() {
+    resetStrengthBars();
+    let poolSize = 0;
+
+    if (uppercaseOption.checked) poolSize += 26;
+    if (lowercaseOption.checked) poolSize += 26;
+    if (numbersOption.checked) poolSize += 10;
+    if (symbolsOption.checked) poolSize += 23;
+    
+    const length = lengthInput.value;
+
+    if (poolSize == 0 || length == 0) {
+        strengthText.innerText = "";
+        return;
+    }
+
+    const entropy = length * Math.log2(poolSize);
+
+    if (entropy < 28) {
+        strengthText.innerText = "TOO WEAK";
+        strengthBars[0].classList.add("red-bar");
+    }
+    else if (entropy < 42) {
+        strengthText.innerText = "WEAK";
+        strengthBars[0].classList.add("orange-bar");
+        strengthBars[1].classList.add("orange-bar");
+    }
+    else if (entropy < 64) {
+        strengthText.innerText = "MEDIUM";
+        strengthBars[0].classList.add("yellow-bar");
+        strengthBars[1].classList.add("yellow-bar");
+        strengthBars[2].classList.add("yellow-bar");
+    }
+    else {
+        strengthText.innerText = "STRONG";
+        strengthBars[0].classList.add("green-bar");
+        strengthBars[1].classList.add("green-bar");
+        strengthBars[2].classList.add("green-bar");
+        strengthBars[3].classList.add("green-bar");
+    }
+}
 
 copyBtn.addEventListener("click", () => {
     password.select();
@@ -21,7 +69,13 @@ copyBtn.addEventListener("click", () => {
 
 lengthInput.addEventListener("input", () => {
     lengthText.innerText = lengthInput.value;
+    updateStrength();
 });
+
+uppercaseOption.addEventListener("change", updateStrength);
+lowercaseOption.addEventListener("change", updateStrength);
+numbersOption.addEventListener("change", updateStrength);
+symbolsOption.addEventListener("change", updateStrength);
 
 generateBtn.addEventListener("click", () => {
     const charPool = [];
